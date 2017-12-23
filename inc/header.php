3dd4d481f73a67fbec70b9465df25867b377c568
mike jones <jones@localhost.localdomain>
2017-12-23 00:42:37 -0600

    fixed header

--- inc/header.php
+++ inc/header.php
@@ -4,8 +4,8 @@
     <head>
         <meta http-equiv="x-ua-compatible" content="ie=edge">
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="css1/bootstrap.css">
-        <link rel="stylesheet" href="css1/style.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
